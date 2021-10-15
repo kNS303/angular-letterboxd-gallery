6f7af5d14ac36885828183224e1dfc0cae65cd78
Alvaro Torres <kanas89@gmail.com>
2021-10-15 19:48:13 +0200

    letterboxd

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'netflix-app'`, () => {
+  it(`should have as title 'letterboxd-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('netflix-app');
+    expect(app.title).toEqual('letterboxd-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('netflix-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('letterboxd-app app is running!');
   });
 });
